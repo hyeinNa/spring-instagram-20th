2d4ec9fca6caae5f6285d7e638d4da29c68f47c5
nhi33 <nhi3373@gmail.com>
2024-09-28 11:45:13 +0900

    chore : 초기 셋팅

---
+++ src/main/java/com/ceos20/spring_instagram/SpringInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.ceos20.spring_instagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInstagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInstagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ceos20/spring_instagram/SpringInstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos20.spring_instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
