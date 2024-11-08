8174affece7c0686dda373b6be1f61a00c5b9aa9
nhi33 <nhi3373@gmail.com>
2024-11-08 16:46:10 +0900

    chore: 필요없는 패키지 삭제

--- src/main/java/com/ceos20/springinstagram/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ceos20.springinstagram.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ceos20.springinstagram.domain.Post;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
--- src/main/java/com/ceos20/springinstagram/repository/ReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ceos20.springinstagram.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.ceos20.springinstagram.domain.Reply;
-
-@Repository
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
